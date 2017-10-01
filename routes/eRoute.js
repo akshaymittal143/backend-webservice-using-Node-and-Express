d0f587cd539689807d6cbc6c198b12eed79bea6e
Akshay Mittal <akshaycanodia@gmail.com>
2017-09-30 19:47:05 -0500

    updated /events/clear

--- routes/eRoute.js
+++ routes/eRoute.js
@@ -7,15 +7,16 @@ module.exports=function (app) {
         .get(eventList.list_all_events)
         .post(eventList.create_a_event)
 
+
+    app.route('/events/clear')
+        .post(eventList.clear_events);
+
     app.route('/events/date')
         .get(eventList.filter_by_dates);
 
     app.route('/events/:eventId')
         .get(eventList.read_a_event)
         .delete(eventList.delete_a_event);
-
-    app.route('/events/:clear')
-        .post(eventList.clear_events);
 
     app.route('/events/summary')
         .get(eventList.get_summary);
